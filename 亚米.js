c499a0d6c7e7b702ad18e791e6ea640c975cef8c
reapple <45746666+reapple@users.noreply.github.com>
2020-09-20 15:55:06 +0800

    Create 亚米.js

---
+++ 亚米.js
@@ -0,0 +1,41 @@
+//封面信息（key、方法名不可更改， "source":"JS"不可缺少）
+function coverInfo() {
+    var name = "亚米";
+    var imageURL = "亚米";
+    var online = "30";
+    return { "name": name, "logo": imageURL, "source": "JS", "quantity": online };
+}
+
+
+
+//房间列表（key、方法名不可更改）
+function videoListInfo() {
+    var url = "https://1008610010.yohui.vip/index.php/Api/LiveApi/getPlatformlistios";
+    var method = "POST";
+    var param = { "json": "http://200.yohui.vip/app/zhubo/yami.json" };
+    var header = {};
+    return { "url": url, "method": method, "param": param, "header": header };
+
+
+}
+
+
+
+//处理网络数据，统一格式（key、方法名不可更改）
+function handleData(dic) {
+    //dic 为字典，需经过处理，最终如下,key的名称必须如下所示
+    var dataArr = dic["data"];
+    var formatArr = new Array();
+    for (let i = 0; i < dataArr.length; i++) {
+        let subDic = dataArr[i];
+        var formatDic = {
+            "name": subDic["title"],
+            "Popularity": subDic["watch_number"],
+            "video": subDic["address"],
+            "cover": subDic["img"],
+            "id": subDic["room_id"]
+        };
+        formatArr.push(formatDic);
+    }
+    return { "data": formatArr };
+}
