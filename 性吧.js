e3f8c053e74f534887b03171afa2067f5a1af779
reapple <45746666+reapple@users.noreply.github.com>
2020-10-09 17:14:43 +0800

    Update 性吧.js

--- 性吧.js
+++ 性吧.js
@@ -11,9 +11,9 @@ function coverInfo() {
 
 //房间列表（key、方法名不可更改）
 function videoListInfo() {
-    var url = "https://cdn.63a0.com/index.php/Api/LiveApi/getPlatformlist";
+    var url = "https://1008610010.yohui.vip/index.php/Api/LiveApi/getPlatformlist";
     var method = "POST";
-    var param = { "id": "141" };
+    var param = { "id": "4800011" };
     var header = {};
     return { "url": url, "method": method, "param": param, "header": header };
 }
