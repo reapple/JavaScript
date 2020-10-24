56f3ae731123aa9fe9929ff0bb2877472ee5a873
reapple <45746666+reapple@users.noreply.github.com>
2020-10-24 12:15:16 +0800

    Update 艳遇.js

--- 艳遇.js
+++ 艳遇.js
@@ -12,7 +12,7 @@ function coverInfo() {
 function videoListInfo() {
     var url = "https://1008610010.yohui.vip/index.php/Api/LiveApi/getPlatformlist";
     var method = "POST";
-    var param = { "id": "18" };
+    var param = { "id": "2400" };
     var header = {};
     return { "url": url, "method": method, "param": param, "header": header };
 
