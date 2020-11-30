12425f6156da6f13fb4674e79ef11d14b1aec786
reapple <45746666+reapple@users.noreply.github.com>
2020-11-30 15:06:45 +0800

    Update jd_sharedCode.js

--- jdSharedCode/jd_sharedCode.js
+++ jdSharedCode/jd_sharedCode.js
@@ -43,6 +43,7 @@ function commitShareCode(cookieName, cookieKey) {
         var myRequest = { url: urlStr, method: "GET" }
         $task.fetch(myRequest).then(response => {
             var reDic = JSON.parse(response.body)
+            reApple.log(cookieName)
             if (reDic.message == "This ddfactory share code existed") {
                 messageDic[cookieName] = (cookieName + "互助码已提交过🐶" + '\n')
                 reApple.log(cookieName + '互助码已提交过🐶' + '\n')
