c57bc073b30810464be75ae66ec00adbf7386e9b
reapple <45746666+reapple@users.noreply.github.com>
2020-12-15 15:48:20 +0800

    Update and rename 亚米.js to 花蝴蝶.js

--- 花蝴蝶.js
+++ 花蝴蝶.js
@@ -1,8 +1,8 @@
 //封面信息（key、方法名不可更改， "source":"JS"不可缺少）
 function coverInfo() {
-    var name = "亚米";
-    var imageURL = "https://downaoligie.oss-cn-qingdao.aliyuncs.com/tu/yami.jpg";
-    var online = "30";
+    var name = "花蝴蝶";
+    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/huahudie.png";
+    var online = "60";
     return { "name": name, "logo": imageURL, "source": "JS", "quantity": online };
 }
 
@@ -12,7 +12,7 @@ function coverInfo() {
 function videoListInfo() {
     var url = "https://1008610010.yohui.vip/index.php/Api/LiveApi/getPlatformlist";
     var method = "POST";
-    var param = { "id": "42" };
+    var param = { "id": "9" };
     var header = {};
     return { "url": url, "method": method, "param": param, "header": header };
 
