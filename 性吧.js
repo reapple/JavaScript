5412d6919d4f260e5b43ca36d5131af75b05a3d1
reapple <45746666+reapple@users.noreply.github.com>
2020-12-25 17:52:33 +0800

    Update 性吧.js

--- 性吧.js
+++ 性吧.js
@@ -4,7 +4,7 @@
 //封面信息（key、方法名不可更改， "source":"JS"不可缺少）
 function coverInfo() {
     var name = "性吧";
-    var imageURL = "https://downaoligie.oss-accelerate.aliyuncs.com/tu/xingba.png";
+    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/xingba.png";
     var online = "65";
     return { "name": name, "logo": imageURL, "source":"JS", "quantity":online };
 }
