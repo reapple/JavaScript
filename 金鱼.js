a88db3bfbc40ad01fa899d831eeb385ba8edb877
reapple <45746666+reapple@users.noreply.github.com>
2020-12-25 17:56:23 +0800

    Update 金鱼.js

--- 金鱼.js
+++ 金鱼.js
@@ -4,7 +4,7 @@
 //封面信息（key、方法名不可更改， "source":"JS"不可缺少）
 function coverInfo() {
     var name = "金鱼";
-    var imageURL = "https://downaoligie.oss-cn-qingdao.aliyuncs.com/tu/jinyu.jpg";
+    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/jinyu.jpg";
     var online = "30";
     return { "name": name, "logo": imageURL, "source":"JS", "quantity":online };
 }
