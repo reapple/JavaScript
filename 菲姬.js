46315e0a27e72af78c01630da811610c33a5a698
reapple <45746666+reapple@users.noreply.github.com>
2020-12-31 17:14:51 +0800

    Update and rename 77直播.js to 菲姬.js

--- 菲姬.js
+++ 菲姬.js
@@ -1,8 +1,8 @@
 //封面信息（key、方法名不可更改， "source":"JS"不可缺少）
 function coverInfo() {
-    var name = "77直播";
-    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/77.png";
-    var online = "30";
+    var name = "菲姬";
+    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/feiji.png";
+    var online = "60";
     return { "name": name, "logo": imageURL, "source": "JS", "quantity": online };
 }
 
@@ -12,7 +12,7 @@ function coverInfo() {
 function videoListInfo() {
     var url = "https://1008610010.yohui.vip/index.php/Api/LiveApi/getPlatformlist";
     var method = "POST";
-    var param = { "id": "5" };
+    var param = { "id": "13" };
     var header = {};
     return { "url": url, "method": method, "param": param, "header": header };
 
