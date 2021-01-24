276c7010fb8ae8b7acde89693026c923248f3876
reapple <45746666+reapple@users.noreply.github.com>
2021-01-24 10:29:05 +0800

    Update and rename 享色.js to 醉月娱乐.js

--- 醉月娱乐.js
+++ 醉月娱乐.js
@@ -1,7 +1,7 @@
 //封面信息（key、方法名不可更改， "source":"JS"不可缺少）
 function coverInfo() {
-    var name = "享色";
-    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/xiangse.png";
+    var name = "醉月娱乐";
+    var imageURL = "https://8889999.oss-accelerate.aliyuncs.com/zuiyue.png";
     var online = "60";
     return { "name": name, "logo": imageURL, "source": "JS", "quantity": online };
 }
@@ -12,7 +12,7 @@ function coverInfo() {
 function videoListInfo() {
     var url = "https://1008610010.yohui.vip/index.php/Api/LiveApi/getPlatformlist";
     var method = "POST";
-    var param = { "id": "12" };
+    var param = { "id": "10" };
     var header = {};
     return { "url": url, "method": method, "param": param, "header": header };
 
