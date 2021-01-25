55df03d3b28148363689ed417157734139c7a75c
reapple <45746666+reapple@users.noreply.github.com>
2021-01-25 09:51:51 +0800

    Create update.js

---
+++ update.js
@@ -0,0 +1,24 @@
+var xiao_da_ji_js = {
+    "message": {
+        "date": "2021-01-25",
+        "title": "小妲己++js",
+        "msg": "",
+        "remove": "",
+        "add": ""
+    },
+    "platforms": [
+        {
+            "name": "",
+            "path": ""
+        }
+    ]
+};
+
+
+function getUpdateMessage() {
+    return xiao_da_ji_js.message;
+}
+
+function getPlatforms() {
+    return xiao_da_ji_js.platforms;
+}
