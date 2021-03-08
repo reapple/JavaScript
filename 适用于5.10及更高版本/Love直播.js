46d59352a87b0300879cff9328316312bac3dfff
reapple <45746666+reapple@users.noreply.github.com>
2021-03-08 21:48:07 +0800

    Update Love直播.js

--- 适用于5.10及更高版本/Love直播.js
+++ 适用于5.10及更高版本/Love直播.js
@@ -17,7 +17,7 @@ function pageInfo() {
 
 //房间列表（key、方法名不可更改）
 function videoListInfo() {
-    var url = "https://lovezbapi.jf-mrx.com.cn/api/public/?auth_key=1614421709-6D3D48B8D732410B83D657CBD2918922-0-6bae93c96ef4ec26d3d14d6068cac7a5&service=Home.getHot";
+    var url = "https://lovezbapi.zjjieneng.cn/api/public/?auth_key=1615209478-6D3D48B8D732410B83D657CBD2918922-0-b382ee43894e336b632d70773cd33f6f&service=Home.getHot";
     var method = "POST";
     var param = { "p": "1" };
     var header = {};
